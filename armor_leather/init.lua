22ae2008670192cce3655669321c03ea0df48f4a
Alex Noir <Varsash@gmail.com>
2022-08-29 12:32:06 +0300

    Add Woven and Leather armor

---
+++ armor_leather/init.lua
@@ -0,0 +1,65 @@
+armor:register_armor(":armor_expanded:helmet_leather", {
+	description = "Leather Helmet",
+	inventory_image = "armor_expanded_inv_helmet_leather.png",
+	groups = {armor_head=1, armor_heal=0, armor_use=800, flammable=1},
+	armor_groups = {fleshy=7},
+	damage_groups = {cracky=3, snappy=2, choppy=2, crumbly=2, level=1},
+})
+
+armor:register_armor(":armor_expanded:chestplate_leather", {
+	description = "Leather Chestplate",
+	inventory_image = "armor_expanded_inv_chestplate_leather.png",
+	groups = {armor_torso=1, armor_heal=0, armor_use=800, flammable=1},
+	armor_groups = {fleshy=12},
+	damage_groups = {cracky=3, snappy=2, choppy=2, crumbly=2, level=1},
+})
+
+armor:register_armor(":armor_expanded:leggings_leather", {
+	description = "Leather Leggings",
+	inventory_image = "armor_expanded_inv_leggings_leather.png",
+	groups = {armor_legs=1, armor_heal=0, armor_use=800, flammable=1},
+	armor_groups = {fleshy=12},
+	damage_groups = {cracky=3, snappy=2, choppy=2, crumbly=2, level=1},
+})
+
+armor:register_armor(":armor_expanded:boots_leather", {
+	description = "Leather Boots",
+	inventory_image = "armor_expanded_inv_boots_leather.png",
+	armor_groups = {fleshy=7},
+	damage_groups = {cracky=3, snappy=2, choppy=2, crumbly=2, level=1},
+	groups = {armor_feet=1, armor_heal=0, armor_use=800, flammable=1},
+})
+
+m = "leather"
+c = "group:leather"
+minetest.register_craft({
+    output = "armor_expanded:helmet_"..m,
+    recipe = {
+        {c, c, c},
+        {c, "", c},
+        {"", "", ""},
+    },
+})
+minetest.register_craft({
+    output = "armor_expanded:chestplate_"..m,
+    recipe = {
+        {c, "", c},
+        {c, c, c},
+        {c, c, c},
+    },
+})
+minetest.register_craft({
+    output = "armor_expanded:leggings_"..m,
+    recipe = {
+        {c, c, c},
+        {c, "", c},
+        {c, "", c},
+    },
+})
+minetest.register_craft({
+    output = "armor_expanded:boots_"..m,
+    recipe = {
+        {c, "", c},
+        {c, "", c},
+    },
+})
---
+++ armor_woven/init.lua
@@ -0,0 +1,65 @@
+armor:register_armor(":armor_expanded:helmet_woven", {
+	description = "Woven Helmet",
+	inventory_image = "armor_expanded_inv_helmet_woven.png",
+	groups = {armor_head=1, armor_heal=0, armor_use=2500, flammable=1},
+	armor_groups = {fleshy=3},
+	damage_groups = {cracky=3, snappy=2, choppy=3, crumbly=2, level=1},
+})
+
+armor:register_armor(":armor_expanded:chestplate_woven", {
+	description = "Woven Chestplate",
+	inventory_image = "armor_expanded_inv_chestplate_woven.png",
+	groups = {armor_torso=1, armor_heal=0, armor_use=2500, flammable=1},
+	armor_groups = {fleshy=8},
+	damage_groups = {cracky=3, snappy=2, choppy=3, crumbly=2, level=1},
+})
+
+armor:register_armor(":armor_expanded:leggings_woven", {
+	description = "Woven Leggings",
+	inventory_image = "armor_expanded_inv_leggings_woven.png",
+	groups = {armor_legs=1, armor_heal=0, armor_use=2500, flammable=1},
+	armor_groups = {fleshy=8},
+	damage_groups = {cracky=3, snappy=2, choppy=3, crumbly=2, level=1},
+})
+
+armor:register_armor(":armor_expanded:boots_woven", {
+	description = "Woven Boots",
+	inventory_image = "armor_expanded_inv_boots_woven.png",
+	armor_groups = {fleshy=3},
+	damage_groups = {cracky=3, snappy=2, choppy=3, crumbly=2, level=1},
+	groups = {armor_feet=1, armor_heal=0, armor_use=2500, flammable=1},
+})
+
+m = "woven"
+c = "group:grass"
+minetest.register_craft({
+    output = "armor_expanded:helmet_"..m,
+    recipe = {
+        {c, c, c},
+        {c, "", c},
+        {"", "", ""},
+    },
+})
+minetest.register_craft({
+    output = "armor_expanded:chestplate_"..m,
+    recipe = {
+        {c, "", c},
+        {c, c, c},
+        {c, c, c},
+    },
+})
+minetest.register_craft({
+    output = "armor_expanded:leggings_"..m,
+    recipe = {
+        {c, c, c},
+        {c, "", c},
+        {c, "", c},
+    },
+})
+minetest.register_craft({
+    output = "armor_expanded:boots_"..m,
+    recipe = {
+        {c, "", c},
+        {c, "", c},
+    },
+})
